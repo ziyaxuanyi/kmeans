de3a8cfb9d1d48d0f83db805362a5b0ee928946d
ziyaxuanyi <2859242752@qq.com>
2020-01-13 10:46:13 +0800

    增加读入训练数据函数

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 import numpy as np
+from os import listdir
 
 def img2vector(filename):
     """
@@ -17,5 +18,30 @@ def img2vector(filename):
     
     return returnvect
 
+def readHandwritingtTrainSet():
+    """
+    read trainSet data
+    return: trainSet [m, 1024]
+            labels
+    """
+    hwLabels = []
+
+    trainingFileList = listdir('digits/trainingDigits')
+    m = len(trainingFileList)
+
+    trainingMat = np.zeros((m, 1024))
+
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+
+        trainingMat[i, :] = img2vector('digits/trainingDigits/%s' % fileNameStr)
+
+    return trainingMat, hwLabels
+
 if __name__ == "__main__":
     print(img2vector('digits/testDigits/0_1.txt'))
+    trainset, label = handwritingClassTest()
+    print(trainset[0], label[0])
